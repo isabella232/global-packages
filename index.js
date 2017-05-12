8f2ef49d384f3845730a0f23f7845f1f61d98e0e
Leo Lamprecht <mindrun@icloud.com>
2017-05-12 17:34:59 +0200

    Use a package for getting the global module path

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
-// Native
-const path = require('path')
-
 // Packages
 const fs = require('fs-promise')
-const which = require('which-promise')
-
-const modulePath = () => new Promise((resolve, reject) => {
-  const npmLocation = which('npm')
-
-  npmLocation.then(dir => {
-    const globalPath = path.join(dir, '../../lib/node_modules')
-    resolve(globalPath)
-  })
-
-  npmLocation.catch(reject)
-})
+const moduleDir = require('global-modules')
 
 module.exports = () => new Promise((resolve, reject) => {
-  const moduleDir = modulePath()
-  moduleDir.catch(reject)
+  // Read contents of global module directory
+  const walker = fs.readdir(moduleDir)
 
-  moduleDir.then(directory => {
-    // Read contents of global module directory
-    const walker = fs.readdir(directory)
+  // If everything went fine, directly return the list of modules
+  walker.then(resolve)
 
-    // If everything went fine, directly return the list of modules
-    walker.then(resolve)
-
-    // If not, hand back the error
-    walker.catch(reject)
-  })
+  // If not, hand back the error
+  walker.catch(reject)
 })
