a9fd612e62d8ca21b6d5821203097fe95d1ea451
Leo Lamprecht <mindrun@icloud.com>
2017-05-12 18:00:40 +0200

    Back to doing the resolving by ourselves

--- index.js
+++ index.js
@@ -3,28 +3,44 @@ const path = require('path')
 
 // Packages
 const fs = require('fs-promise')
-const moduleDir = require('global-modules')
 const {symlinkSync} = require('path-type')
+const which = require('which-promise')
 
-module.exports = () => new Promise((resolve, reject) => {
-  // Read contents of global module directory
-  const walker = fs.readdir(moduleDir)
+const modulePath = () => new Promise((resolve, reject) => {
+  const npmLocation = which('npm')
 
-  // If everything went fine, directly return the list of modules
-  walker.then(modules => {
-    for (const item of modules) {
-      const index = modules.indexOf(item)
-      const fullPath = path.join(moduleDir, item)
-
-      modules[index] = {
-        name: item,
-        linked: symlinkSync(fullPath),
-        path: fullPath
-      }
-    }
-    resolve(modules)
+  npmLocation.then(dir => {
+    const globalPath = path.join(dir, '../../lib/node_modules')
+    resolve(globalPath)
   })
 
-  // If not, hand back the error
-  walker.catch(reject)
+  npmLocation.catch(reject)
+})
+
+module.exports = () => new Promise((resolve, reject) => {
+  const moduleDir = modulePath()
+  moduleDir.catch(reject)
+
+  moduleDir.then(directory => {
+    // Read contents of global module directory
+    const walker = fs.readdir(directory)
+
+    // If everything went fine, directly return the list of modules
+    walker.then(modules => {
+      for (const item of modules) {
+        const index = modules.indexOf(item)
+        const fullPath = path.join(directory, item)
+
+        modules[index] = {
+          name: item,
+          linked: symlinkSync(fullPath),
+          path: fullPath
+        }
+      }
+      resolve(modules)
+    })
+
+    // If not, hand back the error
+    walker.catch(reject)
+  })
 })
