5e03df7dd8e9c8a4349896de3478d8c80a2a852c
Leo Lamprecht <mindrun@icloud.com>
2017-06-06 14:17:34 +0200

    Ensure that the module directory is right cross-platform

--- index.js
+++ index.js
@@ -4,43 +4,27 @@ const path = require('path')
 // Packages
 const fs = require('fs-promise')
 const {symlinkSync} = require('path-type')
-const which = require('which-promise')
-
-const modulePath = () => new Promise((resolve, reject) => {
-  const npmLocation = which('npm')
-
-  npmLocation.then(dir => {
-    const globalPath = path.join(dir, '../../lib/node_modules')
-    resolve(globalPath)
-  })
-
-  npmLocation.catch(reject)
-})
+const directory = require('global-modules')
 
 module.exports = () => new Promise((resolve, reject) => {
-  const moduleDir = modulePath()
-  moduleDir.catch(reject)
+  // Read contents of global module directory
+  const walker = fs.readdir(directory)
 
-  moduleDir.then(directory => {
-    // Read contents of global module directory
-    const walker = fs.readdir(directory)
+  // If everything went fine, directly return the list of modules
+  walker.then(modules => {
+    for (const item of modules) {
+      const index = modules.indexOf(item)
+      const fullPath = path.join(directory, item)
 
-    // If everything went fine, directly return the list of modules
-    walker.then(modules => {
-      for (const item of modules) {
-        const index = modules.indexOf(item)
-        const fullPath = path.join(directory, item)
-
-        modules[index] = {
-          name: item,
-          linked: symlinkSync(fullPath),
-          path: fullPath
-        }
+      modules[index] = {
+        name: item,
+        linked: symlinkSync(fullPath),
+        path: fullPath
       }
-      resolve(modules)
-    })
-
-    // If not, hand back the error
-    walker.catch(reject)
+    }
+    resolve(modules)
   })
+
+  // If not, hand back the error
+  walker.catch(reject)
 })
