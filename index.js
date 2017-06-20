8e7852728d2272f8b0ef9345cfb1e8dc32c02927
Leo Lamprecht <mindrun@icloud.com>
2017-06-20 13:34:26 +0200

    Use `await` everywhere

--- index.js
+++ index.js
@@ -6,25 +6,29 @@ const fs = require('fs-promise')
 const {symlinkSync} = require('path-type')
 const directory = require('global-modules')
 
-module.exports = () => new Promise((resolve, reject) => {
-  // Read contents of global module directory
-  const walker = fs.readdir(directory)
+module.exports = async () => {
+  let modules
 
-  // If everything went fine, directly return the list of modules
-  walker.then(modules => {
-    for (const item of modules) {
-      const index = modules.indexOf(item)
-      const fullPath = path.join(directory, item)
-
-      modules[index] = {
-        name: item,
-        linked: symlinkSync(fullPath),
-        path: fullPath
-      }
+  try {
+    modules = await fs.readdir(directory)
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return []
     }
-    resolve(modules)
-  })
 
-  // If not, hand back the error
-  walker.catch(reject)
-})
+    throw err
+  }
+
+  for (const item of modules) {
+    const index = modules.indexOf(item)
+    const fullPath = path.join(directory, item)
+
+    modules[index] = {
+      name: item,
+      linked: symlinkSync(fullPath),
+      path: fullPath
+    }
+  }
+
+  return modules
+}
